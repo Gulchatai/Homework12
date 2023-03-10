99830e4080ad2d8df7c2b5953fb4a230a96151de
Gulchatai <sgalinaval@gmail.com>
2023-03-10 04:51:44 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашнее задание № 12");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int numberOfYear = 240;
+        checkStatusYear(numberOfYear);
+    }
+
+    public static void checkStatusYear(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) && (year != 0)) {
+            System.out.println(year + " год - високосный год");
+        } else {
+            System.out.println(year + " год - невисокосный год");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        //int currentYear = LocalDate.now().getYear(); НЕ ПОЛУЧАЕТСЯ ПОКА ИСПОЛЬЗОВАТЬ ПРОШУ ПРОКОНСУЛЬТИРОВАТЬ
+        // ВИЖУ ЧТО ПОДГРУЗИЛОСЬ ЧТО-ТО СВЕРХУ
+        byte type = 1;
+        int yearReleaseDevice = 1000;
+        selectTypeOfSystem(type, yearReleaseDevice);
+    }
+
+    public static int selectTypeOfSystem(byte typeOfSystem, int year) {
+        if (typeOfSystem == 0 && year < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (typeOfSystem == 0 && year >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (typeOfSystem != 0 && year < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        return (typeOfSystem);
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int deliveryDistance = 88;
+        calculateQuantityOfDays(deliveryDistance);
+    }
+
+    public static int calculateQuantityOfDays(int distance) {
+        int days = 1;
+        if (distance <= 20) {
+            System.out.println("Потребуется дней: " + days);
+        }
+        if (distance > 20 && distance <= 60) {
+            days = days + 1;
+            System.out.println("Потребуется дней: " + days);
+        }
+        if (distance > 60 && distance <= 100) {
+            days = days + 2;
+            System.out.println("Потребуется дней: " + days);
+        }
+        if (distance > 100) {
+            System.out.println("Доставка невозможна");
+        }
+        return (distance);
     }
 }
